8ec3546af69c0ba8c0a0fc5b22d4df5fc926b973
karenandrea1369 <karenandrea12204@gmail.com>
2020-05-04 22:22:36 -0500

    Updates

---
+++ precache-manifest.ff2cfa36f6d9d13dd0780ad3fd950690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbf3850a1379f2cc78e332615ace5a1",
+    "url": "/spidermanapp/index.html"
+  },
+  {
+    "revision": "79f65367bfcfb37894ee",
+    "url": "/spidermanapp/static/css/main.5d3a3746.chunk.css"
+  },
+  {
+    "revision": "5b99492238aae466c117",
+    "url": "/spidermanapp/static/js/2.244c58fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spidermanapp/static/js/2.244c58fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f65367bfcfb37894ee",
+    "url": "/spidermanapp/static/js/main.f32a9dd9.chunk.js"
+  },
+  {
+    "revision": "eb308b663e82d0b9c537",
+    "url": "/spidermanapp/static/js/runtime-main.3111c0db.js"
+  }
+]);
